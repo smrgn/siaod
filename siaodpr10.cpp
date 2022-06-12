d2946db970ea164ddc207c0fb4f0d36d548b6bc5
Viktoriia <100041410+smrgn@users.noreply.github.com>
2022-06-12 18:34:42 +0300

    Add files via upload

---
+++ siaodpr10.cpp
@@ -0,0 +1,166 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+#include <vector>
+#include<regex>
+#include <Windows.h>
+#include <unordered_map>
+using namespace std;
+
+vector<int> prefix_func(const string& s) {
+    vector<int> p(s.length());
+
+    int k = 0;
+    p[0] = 0;
+    for (int i = 1; i < s.length(); ++i) {
+        while (k > 0 && s[k] != s[i]) {
+            k = p[k - 1];
+        }
+        if (s[k] == s[i]) {
+            ++k;
+        }
+        p[i] = k;
+    }
+    return p;
+}
+
+int find(string& s, string& t, unsigned long long &c) {
+    c++;
+    if (s.length() < t.length()) {
+        return -1;
+    }
+    c++;
+    if (!t.length()) {
+        return s.length();
+    }
+
+    typedef unordered_map<char, int> TStopTable;
+    typedef unordered_map<int, int> TSufficsTable;
+    TStopTable stop_table;
+    TSufficsTable suffics_table;
+
+    c++;
+    for (int i = 0; i < t.length(); ++i) {
+        c++;
+        stop_table[t[i]] = i;
+    }
+
+    string rt(t.rbegin(), t.rend());
+    vector<int> p = prefix_func(t), pr = prefix_func(rt);
+    c++;
+    for (int i = 0; i < t.length() + 1; ++i) {
+        c++;
+        suffics_table[i] = t.length() - p.back();
+    }
+
+    c++;
+    for (int i = 1; i < t.length(); ++i) {
+        c++;
+        int j = pr[i];
+        suffics_table[j] = min(suffics_table[j], i - pr[i] + 1);
+    }
+
+    c++;
+    for (int shift = 0; shift <= s.length() - t.length();) {
+        c++;
+        int pos = t.length() - 1;
+
+        while (t[pos] == s[pos + shift]) {
+            c++;
+            if (pos == 0)
+            {
+                c++;
+                return shift;
+            }
+            --pos;
+        }
+
+        if (pos == t.length() - 1) {
+            c++;
+            TStopTable::const_iterator stop_symbol = stop_table.find(s[pos + shift]);
+            int stop_symbol_additional = pos - (stop_symbol != stop_table.end() ? stop_symbol->second : -1);
+            shift += stop_symbol_additional;
+        }
+        else {
+            c++;
+            shift += suffics_table[t.length() - pos - 1];
+        }
+    }
+
+    return -1;
+}
+
+
+
+int quantity_of_substrings(string str, string& substr, unsigned long long &c)
+{
+    int pos = 0;
+    int q = 0;
+    while (pos != -1)
+    {
+        pos = find(str, substr, c);
+        if (pos != -1)
+        {
+            q++;
+            str.erase(0, pos + substr.length());
+        }
+    }
+    return q;
+}
+
+string generate_random_text(int n)
+{
+    vector<string> words = { "головная боль 09.03.21", "зубная боль 03.10.21", "сдача анализов 19.02.19", "зубная боль 24.10.21", "сдача анализов 28.04.19", " узи 10.10.21" };
+    vector<string> names = { "СВВааааааа", "ВАВооооооо", "СДАууууууу", "ОВАппппппп", "ММПггггггг" };
+    int w, r;
+    string text = "";
+    srand(time(0));
+    for (int i = 0; i < n/25; i++)
+    {
+        r = rand() % words.size();
+        w = rand() % names.size();
+        text += names[w] + " " + words[r] + " ";
+    }
+    return text;
+}
+
+
+int main() {
+	setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int exs;
+    string numb, info, s;
+    unsigned long long c = 0;
+    smatch m;
+    regex e("([0-9a-z_]+)([\-*\/\+])");
+    cout << "Выберете задание:\n1 - найти, сколько раз пациент обращался в клинику;\n2 - Регулярные выражения.\n";
+    cin >> exs;
+    switch (exs) {
+    case 1:
+        cout << "Все пациенты: СВВааааааа, ВАВооооооо, СДАууууууу, ОВАппппппп, ММПггггггг.\n";
+        cout << "Введите номер карты пациента: ";
+        getline(cin, numb);
+        getline(cin, numb);
+        for (int n = 100000; n <= 500000; n += 100000) {
+            cout << "\nn = " << n << " " << "Количество раз = ";
+            info = generate_random_text(n);
+            cout << quantity_of_substrings(info, numb, c);
+            cout << "\nC + M = " << c << endl;
+            c = 0;
+        }
+        break;
+
+    case 2:
+        cout << "Введети алгебраическое выражение: ";
+
+        getline(cin, s);
+        getline(cin, s);
+
+        cout << regex_replace(s, e, "0$2");
+        
+        break;
+    }
+	return 0;
+}
---
+++ СИАОДПР9.cpp
@@ -0,0 +1,169 @@
+﻿#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <time.h> 
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <chrono> 
+#include <random>
+#pragma warning(disable: 4996)
+using namespace std;
+
+struct branch
+{
+    char name[16];
+    int code;
+};
+
+struct branch_vec
+{
+	int code;
+	string name;
+};
+
+string create_name(vector<string> names)
+{
+	int n = names.size();
+	return names[rand() % n];
+}
+
+int search_line(vector< branch_vec> table, int find_code)
+{
+	int flag = 0;
+	for (int i = 0; i < table.size(); i++)
+	{
+		if (find_code == table[i].code)
+		{
+			cout << "Линейный поиск: " << table[i].code << " " << table[i].name << endl;
+			flag = 1;
+		}
+	}
+	if (!flag) {
+		cout << "Специальность с таким кодом отсутствует. Повторите ввод: ";
+	}
+	return flag;
+}
+
+template<typename T>
+int binary_search(const vector<T>& vec, T& item, int s1, int s2) {
+	if (s1 > s2)
+		return -1;
+
+	auto middle = (s1 + s2) / 2;
+
+	if (item == vec.at(middle))
+		return middle;
+
+	if (item > vec.at(middle))
+		return binary_search(vec, item, middle + 1, s2);
+	else
+		return binary_search(vec, item, s1, middle - 1);
+}
+
+template<typename T>
+int search_bin(const vector<T>& vec, T& item) {
+	return binary_search(vec, item, 0, vec.size() - 1);
+}
+
+int BinSearch(vector<int> arr, int key)
+{
+	int l = 0; // нижняя граница
+	int u = arr.size() - 1; // верхняя граница
+
+	while (l <= u) {
+		int m = (l + u) / 2;
+		if (arr[m] == key) return m;
+		if (arr[m] < key) l = m + 1;
+		if (arr[m] > key) u = m - 1;
+	}
+	return -1;
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+
+	vector<branch_vec> table; 
+	vector<int> code_table; 
+	vector<branch_vec> table_sorted; 
+	branch_vec br_vec;
+	vector <string> names = { "РТУ МИРЭА", "МГУ", "ВШЭ", "РАНХиГС", "МГТУ", "МПГУ", "ТвГУ", "НГУ", "СПбГУ", "ИТМО", "РГГУ" };
+
+	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+	default_random_engine e(seed);
+
+	int n, find_code;
+	cout << "Введите количество специальностей: " << endl;
+	cin >> n;
+
+	cout << "Пожалуйста, ожидайте, идёт создание файла...\n";
+
+	vector <int> n_vec;
+	for (int i = 0; i < n; i++)
+	{
+		n_vec.push_back(i);
+	}
+	shuffle(n_vec.begin(), n_vec.end(), e);
+
+	ofstream binFile("ninepr.bin", ios::binary);
+
+	for (int i = 0; i < n; i++)
+	{
+		branch* br = new branch;
+		br->code = 100001 + n_vec[i] * 17;
+		if (i == n - 1)
+		{
+			br->code = 100000;
+		}
+		strcpy(br->name, create_name(names).c_str());
+		binFile.write((char*)br, sizeof(branch));
+	}
+	binFile.close();
+	branch* br = new branch;
+	ifstream binFile2("ninepr.bin", ios::binary);
+	while (binFile2.read((char*)br, sizeof(branch)))
+	{
+		br_vec.code = br->code;
+		br_vec.name = br->name;
+		table.push_back(br_vec);
+		code_table.push_back(br->code);
+	}
+	sort(code_table.begin(), code_table.end());
+	for (int i = 0; i < code_table.size(); i++)
+	{
+		for (int j = 0; j < table.size(); j++)
+		{
+			if (table[j].code == code_table[i])
+			{
+				br_vec.code = table[j].code;
+				br_vec.name = table[j].name;
+
+				table_sorted.push_back(br_vec);
+			}
+		}
+	}
+	cout << "Код специальности" << "   " << "Название вуза\n";
+	for (int i = 0; i < table_sorted.size(); i++)
+	{
+		cout << table_sorted[i].code << "              " << table_sorted[i].name << endl;
+	}
+
+	cout << "\nВведите код специальности : ";
+	cin >> find_code;
+	clock_t start;
+	double duration;
+	start = clock();
+	while (!search_line(table, find_code)) {
+		cin >> find_code;
+	}
+	duration = (clock() - start) / (double)CLOCKS_PER_SEC;
+	cout << "Линейный поиск " << n << " элементов: " << duration << "\n";
+
+	start = clock();
+	cout << "Бинарный поиск: ";
+	int m = search_bin(code_table, find_code);
+	cout << table_sorted[m].code << " " << table_sorted[m].name << endl;
+	duration = (clock() - start) / (double)CLOCKS_PER_SEC;
+	cout << "Бинарный поиск " << n << " элементов: " << duration << '\n';
+}
