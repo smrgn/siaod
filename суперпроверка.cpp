30206029db300f9d4bb707d7d174f01a2c8d60e9
Viktoriia <100041410+smrgn@users.noreply.github.com>
2023-07-06 11:59:12 +0300

    Add files via upload

---
+++ суперпроверка.cpp
@@ -0,0 +1,27 @@
+﻿// Проверка на введение целочисленного значения, а не какого-то другого элемента
+
+
+#include <iostream>
+using namespace std;
+
+int Inputint(int m, int M) {
+    for (;;) {
+        float valuea;
+        cout << "Введите число. \n";
+        if ((cin >> valuea).good() && (m <= valuea) && (valuea <= M) && (floor(valuea) == valuea)) {
+            return valuea;
+            break;
+        }
+        if (cin.fail()) {
+            cin.clear();
+            cout << "Неверный ввод, повторите. \n";
+
+        }
+        else {
+            cout << "Число вне допустимого диапазона значений. Повторите ввод. \n";
+
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    }
+}
