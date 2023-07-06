4a05d5de8b959c518c23af17d1a7328798a719c9
Viktoriia <100041410+smrgn@users.noreply.github.com>
2023-07-06 11:34:18 +0300

    Add files via upload

---
+++ siaodpr3.cpp
@@ -0,0 +1,159 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <Windows.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <fstream>
+using namespace std;
+
+
+int in_vec(vector<pair<char, int>> offsets, char c) {
+	for (pair<char, int> p : offsets)
+		if (p.first == c)
+			return p.second;
+	return 0;
+}
+
+vector<pair<char, int>> get_list_offsets(string pattern) { 
+	vector<pair<char, int>> offsets;
+	int size = pattern.size();
+	for (int i = size - 2; i >= 0; i--) {
+		if (!in_vec(offsets, pattern[i])) {
+			offsets.push_back(pair<char, int>{pattern[i], size - i - 1}); //добавляем в массив смещений (элемент, смещение (число до последнего символа))
+		}
+	}
+	if (!in_vec(offsets, pattern[size - 1]))
+		offsets.push_back(pair<char, int>{pattern[size - 1], size});
+	return offsets;
+}
+
+bool boyer_mur(string pattern, string s) { 
+	vector<pair<char, int>> offsets = get_list_offsets(pattern);
+	const int p_size = pattern.size();
+	const int s_size = s.size();
+	if (s_size < p_size)
+		return false;
+	int ind = p_size - 1;
+
+	while (ind < s_size) {
+		bool fl = true;
+		for (int i = p_size - 1, j = ind; i >= 0; i--, j--) {
+			if (s[j] != pattern[i]) {
+				if (int d = in_vec(offsets, s[j])) {
+					ind += d;
+					cout << ' ' << d;
+				}
+				else {
+					ind += p_size;
+					cout << ' ' << p_size;
+				}
+				fl = false;
+				break;
+			}
+		}
+		if (fl)
+			return true;
+	}
+	return false;
+}
+
+int task2() {
+	string buf_line;
+	string pattern; 
+	ifstream file("test2.txt");
+	if (!file.is_open())
+		return 1;
+	getline(file, pattern);
+	while (getline(file, buf_line)) {
+		cout << "............................." << endl << pattern << endl << buf_line << endl;
+		cout << "offsets:";
+		if (boyer_mur(pattern, buf_line))
+			cout << endl << "YES" << endl;
+		else
+			cout << endl << "NO" << endl;
+	}
+	file.close();
+}
+
+int task1() {
+	string text = "apple.application!juice:cat;camale?july."; // данный текст
+	string subline; //подстрока, которую будем искать
+	string word; 
+
+	cout << "Введите подстроку:\n";
+	getline(cin, subline); //считывание подстроки
+	getline(cin, subline);
+
+	vector <string> arr; //создание массива, куда будут записываться слова
+	vector <string> arr_temp;
+
+	for (int i = 0; i < text.size(); i++) {
+		if (isalpha(text[i])) {
+			word += text[i];
+		}
+		else {
+			arr_temp.push_back(word);
+			word = "";
+		}
+	}
+	string cur_word;
+	int flag;
+	for (int i = 0; i < arr_temp.size(); i++) {
+		flag = 1;
+		cur_word = arr_temp[i];
+		if (cur_word.size() >= subline.size()) {
+			for (int j = 0; j < subline.size(); j++) {
+				if (subline[j] != cur_word[j]) {
+					flag = 0;
+				}
+			}
+			if (flag) {
+				arr.push_back(cur_word);
+			}
+		}
+	}
+	if (arr.size() == 0) {
+		cout << "Таких слов не нашлось.";
+		return 1;
+	}
+	for (int i = 0; i < arr.size(); i++) {
+		cout << arr[i] << " ";
+	}
+	return 0;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int numb = 0;
+
+	cout << "1) Дан текст, разделенных знаками препинания.\nСформировать";
+	cout << "массив из слов, в которых заданная подстрока размещается с первой позиции.\n\n";
+	cout << "2) В текстовом файле хранятся входные данные:\nна первой";
+	cout << "сроке – подстрока(образец)длиной не более 17 символов для";
+	cout << "поиска в тексте;\nсо второй строки – текст(строка), в котором";
+	cout << "осуществляется поиск образца.\nСтрока, в которой надо искать, не";
+	cout << "ограниченна по длине.\nПрименяя алгоритм Бойера - Мура с";
+	cout << "турбосдвигом вывести индексы строки, на которые смещается";
+	cout << "алгоритм при поиске вхождения образца.\n";
+	cout << "\nВыберете задание, которое хотите выполнить:\n";
+	cin >> numb;
+
+	while (numb != 1 && numb != 2) {
+		cout << "Неверный ввод. Повторите: ";
+		cin >> numb;
+	}
+	switch (numb) {
+	case 1: 
+		task1();
+		break;
+	case 2:
+		task2();
+		break;
+	}
+	return 0;
+}
---
+++ siaodpr4.cpp
@@ -0,0 +1,197 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+struct BinTree {
+    char value;
+    BinTree* left;
+    BinTree* right;
+};
+
+BinTree* addnode(BinTree* tree, char x) {
+    if (tree == NULL) { // Если дерева нет, то формируем корень
+        tree = new BinTree; // память под узел
+        tree->value = x;   // поле данных
+        tree->left = NULL;
+        tree->right = NULL; // ветви инициализируем пустотой
+    }
+    else if (x <= tree->value) {  // условие добавление левого потомка
+        tree->left = addnode(tree->left, x);
+    }
+    else {  // условие добавление правого потомка
+        tree->right = addnode(tree->right, x);
+    }
+    return(tree);
+}
+
+BinTree* createTree(int n) {
+    vector<char> vec;
+    for (int i = 0; i < n; i++) {
+        cout << "Enter the " << i + 1 << " value: ";
+        char value;
+        cin >> value;
+        vec.push_back(value);
+    }
+
+    cout << "\nWaiting. The tree is created...\n";
+
+    vector<char> v = vec;
+    sort(v.begin(), v.end()); //сортируем вектор
+    char root = v[v.size() / 2];
+    int index = v.size() / 2;
+    BinTree* tree = NULL;
+    tree = addnode(tree, root);
+    vector<char>::iterator position = find(vec.begin(), vec.end(), root);
+    if (position != vec.end()) // == vec.end() means the element was not found
+        vec.erase(position);
+    while (!vec.empty()) {
+        tree = addnode(tree, vec[0]);
+        vec.erase(vec.begin());
+    }
+
+    return tree;
+}
+
+void output(BinTree* root, int space = 0) {
+    if (!root)
+        return;
+    enum { COUNT = 2 };
+    space += COUNT;
+    output(root->right, space);
+    for (int i = COUNT; i < space; ++i)
+        cout << "  ";
+    cout << root->value << endl;
+    output(root->left, space);
+}
+
+int sumLeaf(BinTree* tree) {
+    if (tree == NULL) {
+        return 0;
+    }
+    if (tree->left == NULL && tree->right == NULL) {
+        return tree->value;
+    }
+    int leftPart = sumLeaf(tree->left);
+    int rightPart = sumLeaf(tree->right);
+    return leftPart + rightPart;
+}
+
+int findHeight(BinTree* tree) {
+    if (tree == NULL) {
+        return 0;
+    }
+    int leftHeight = findHeight(tree->left);
+    int rightHeight = findHeight(tree->right);
+    return max(leftHeight, rightHeight) + 1;
+}
+
+void reversBypass(BinTree* tree) {
+    if (tree == NULL) {
+        return;
+    }
+    reversBypass(tree->left);
+    reversBypass(tree->right);
+    cout << tree->value << " ";
+}
+
+void symmetricBypass(BinTree* tree) {
+    if (tree == NULL) {
+        return;
+    }
+    symmetricBypass(tree->left);
+    cout << tree->value << " ";
+    symmetricBypass(tree->right);
+}
+
+int main() {
+    BinTree* tree;
+    cout << "Do you wanna create the tree yourself or automatically?";
+    cout << "\n1) yorself.\n2) automatically.\n";
+    int type;
+    cin >> type;
+    while (type <= 0 || type > 2) {
+        cout << "Error. Retype: ";
+        cin >> type;
+    }
+    if (type == 2) {
+        tree =
+            new BinTree{ 'g',
+                new BinTree{'c',
+                    new BinTree{'a', 0,
+                        new BinTree{'b', 0, 0}
+                    },
+                    new BinTree{'d', 0,
+                        new BinTree{'f', new BinTree{'e', 0, 0}, 0},
+                    },
+                },
+                new BinTree{'i',
+                    new BinTree{'h', 0, 0},
+                    new BinTree{'k', new BinTree{'j', 0, 0}, new BinTree{'e', 0, 0}},
+                },
+        };
+    }
+    else {
+        int n;
+        cout << "Enter the amount of values: ";
+        cin >> n;
+        while (n <= 0) {
+            cout << "Error. Retype: ";
+            cin >> n;
+        }
+        tree = createTree(n);
+    }
+
+    cout << "\n---Tree---" << endl;
+    output(tree);
+    
+    int exc = 1;
+    while (exc != 0) {
+        cout << "Choose what you wanna do: \n";
+        cout << "1) Find height;\n" << "2) Find leaf sum;\n";
+        cout << "3) Make a revers bypass;\n" << "4) Make a symmetric bypass;\n";
+        cout << "5) Add item;\n" << "0) for exit.\n";
+        cin >> exc;
+        while (exc < 0 || exc > 5) {
+            cout << "Error. Retype: ";
+            cin >> exc;
+        }
+        switch (exc) {
+        case 1: {
+            cout << "Height: " << findHeight(tree) << endl;
+            break;
+        }
+        case 2: {
+            cout << "Sum of leaves: " << sumLeaf(tree) << endl;
+            break;
+        }
+        case 3: {
+            cout << "Revers Bypass: ";
+            reversBypass(tree);
+            cout << endl;
+            break;
+        }
+        case 4: {
+            cout << "Symmetric Bypass: ";
+            symmetricBypass(tree);
+            cout << endl;
+            break;
+        }
+        case 5: {
+            cout << "Enter item which you wanna add: ";
+            char item;
+            cin >> item;
+            addnode(tree, item);
+
+            cout << "\n---Tree---" << endl;
+            output(tree);
+            break;
+        }
+        case 0: {
+            cout << "Program is ending....";
+            break;
+        }
+        }
+    }
+    return 0;
+}
